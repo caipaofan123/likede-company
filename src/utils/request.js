2c206a9c37ffdefd89cc02342176bcd88a0336d4
yourname <969729240@qq.com>
2022-08-08 09:52:59 +0800

    提交

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,29 +1,30 @@
 // 导出一个axios的实例  而且这个实例要有请求拦截器 响应拦截器
 import store from '@/store'
 import axios from 'axios'
-import { getTokenTime } from '@/utils/auth'
+// import { getTokenTime } from '@/utils/auth'
 // import { Message } from 'element-ui'
 const service = axios.create({
   baseURL: 'http://localhost:9528/',
   timeout: 5000
 }) // 创建一个axios的实例
 
-const isTimeout = () => {
-  const currentTime = Date.now()
-  const tokenTime = getTokenTime()
-  const timeout = 10 * 60 * 1000
-  return currentTime - tokenTime > timeout
-}
+// const isTimeout = () => {
+//   const currentTime = Date.now()
+//   const tokenTime = getTokenTime()
+//   const timeout = 10 * 60 * 1000
+//   return currentTime - tokenTime > timeout
+// }
 
 service.interceptors.request.use(async(config) => {
   if (store.state.user.token) {
-    if (isTimeout()) {
-      await store.dispatch('user/logout')
-      router.push('/login')
-      return Promise.reject(new Error('登录过期'))
-    } else {
-      config.headers['Authorization'] = `${store.state.user.token}`
-    }
+    config.headers['Authorization'] = `${store.state.user.token}`
+
+    // if (isTimeout()) {
+    //   await store.dispatch('user/logout')
+    //   router.push('/login')
+    //   return Promise.reject(new Error('登录过期'))
+    // } else {
+    // }
   }
   return config
 }) // 请求拦截器
