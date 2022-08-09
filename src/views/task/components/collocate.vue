f073e396c0a50f16ca645bcfc67b3a39be6b2ef4
zhangyu <2863680540@qq.com>
2022-08-09 22:40:57 +0800

    修改

--- src/views/task/components/collocate.vue
+++ src/views/task/components/collocate.vue
@@ -28,11 +28,8 @@ export default {
   data() {
     return {
       num: "",
-<<<<<<< HEAD
-      alertValue: "",
-=======
       alertValue: 0,
->>>>>>> feature/task
+
     };
   },
   props: {
@@ -66,18 +63,15 @@ export default {
     },
     // 自动补货工单阈值
     async getReplenish() {
-<<<<<<< HEAD
+
       const data = await getReplenishApi({
         alertValue: data.alertValue,
       });
       console.log(res);
-=======
-      const res = await getReplenishApi({
-        alertValue: this.num,
-      });
+      
       this.alertValue = res.data;
       this.$emit("update:visible", false);
->>>>>>> feature/task
+
     },
   },
   updated() {},
