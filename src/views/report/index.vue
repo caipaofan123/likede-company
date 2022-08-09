9aca3a6cd7b7d846906fb964dc618c7240b1a845
caipaofan <954698621@qq.com>
2022-08-09 23:44:14 +0800

    修改

--- src/views/report/index.vue
+++ src/views/report/index.vue
@@ -1,27 +1,32 @@
 <template>
-  <div>
-report
+  <div class="dashboard-container">
+    <div class="dashboard-text">
+      <h3>恭喜发财</h3>
+      <img src="../../assets/images/asd.jpeg" alt=""></div>
   </div>
 </template>
 
 <script>
+
 export default {
-  data () {
-    return {
+  name: 'Dashboard'
 
-    }
-  },
-
-  created () {
-
-  },
-
-  methods: {
-
-  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+    color: gold
+  }
 
+}
+img{
+    width:600px
+  }
 </style>
